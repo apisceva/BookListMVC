6dc41abba2bcd670041eeed69e62e0706dd6fdd9
apisceva <aljona.gromova@gmail.com>
2022-06-29 04:42:08 -0400

    Book class added

--- BookListMVC/BookListMVC/Models/Book.cs
+++ BookListMVC/BookListMVC/Models/Book.cs
@@ -1,6 +1,15 @@
-﻿namespace BookListMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookListMVC.Models
 {
     public class Book
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
     }
 }
